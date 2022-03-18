9f430e553488585d1895fa4b5e8bb9868f9c4ac4
Samrid Somboon <samrids@gmail.com>
2022-03-18 12:48:56 +0700

    first commit

---
+++ Project1.dpr
@@ -0,0 +1,17 @@
+﻿program Project1;
+
+uses
+  Vcl.Forms,
+  Unit2 in 'Unit2.pas' {Form1},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Emerald Light Slate');
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ README.md
@@ -0,0 +1,5 @@
+# Delphi_TStrings
+  Delphi: How to get data from TStrings
+  This demo show you, How to get the data from TStrings then Split
+  and show you how to work with TControlList
+  Data source: https://www.tiobe.com/tiobe-index/:
